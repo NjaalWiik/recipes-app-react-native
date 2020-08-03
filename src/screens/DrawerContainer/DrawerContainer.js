9a2f9e4fb930bd67aaea5176146b64ed7a22eb9d
Njaal Wiik <njaal.wiik@gmail.com>
2020-08-03 17:07:57 +0200

    Changing

--- src/screens/DrawerContainer/DrawerContainer.js
+++ src/screens/DrawerContainer/DrawerContainer.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { View } from 'react-native';
-import PropTypes from 'prop-types';
-import styles from './styles';
-import MenuButton from '../../components/MenuButton/MenuButton';
+import React from "react";
+import { View } from "react-native";
+import PropTypes from "prop-types";
+import styles from "./styles";
+import MenuButton from "../../components/MenuButton/MenuButton";
 
 export default class DrawerContainer extends React.Component {
   render() {
@@ -12,25 +12,17 @@ export default class DrawerContainer extends React.Component {
         <View style={styles.container}>
           <MenuButton
             title="HOME"
-            source={require('../../../assets/icons/home.png')}
+            source={require("../../../assets/icons/home.png")}
             onPress={() => {
-              navigation.navigate('Home');
+              navigation.navigate("Home");
               navigation.closeDrawer();
             }}
           />
           <MenuButton
             title="CATEGORIES"
-            source={require('../../../assets/icons/category.png')}
+            source={require("../../../assets/icons/category.png")}
             onPress={() => {
-              navigation.navigate('Categories');
-              navigation.closeDrawer();
-            }}
-          />
-          <MenuButton
-            title="SEARCH"
-            source={require('../../../assets/icons/search.png')}
-            onPress={() => {
-              navigation.navigate('Search');
+              navigation.navigate("Categories");
               navigation.closeDrawer();
             }}
           />
@@ -42,6 +34,6 @@ export default class DrawerContainer extends React.Component {
 
 DrawerContainer.propTypes = {
   navigation: PropTypes.shape({
-    navigate: PropTypes.func.isRequired
-  })
+    navigate: PropTypes.func.isRequired,
+  }),
 };
